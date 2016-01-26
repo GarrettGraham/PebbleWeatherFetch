6b6628b1ad1a6cf91fa652f3e0ce09bf8cc33517
Garrett Lee Graham Anderson <ganderson7197@icloud.com>
2016-01-26 10:18:06 -0500

    Forecast data
    working through the tutorial on getting forecast data from the api. commented out the previous bit of code that just displayed current conditions for a specific location. Now I am working on getting forecast data for orlando but in the same project file so I can keep everything together without making new projects. I tried to look into creating a new branch on github from cloudpebble but that didnt seem to work. now going to see about pulling the repo onto my local and then work off that in order to create a new branch. hope that doesnt mess up the work flow.

--- src/app.js
+++ src/app.js
@@ -7,44 +7,91 @@
 var UI = require('ui');
 var ajax = require('ajax');
 
-// create a card with title and subtitle
-var card = new UI.Card({
-  title: 'Weather',
-  subtitle: 'Fetching...'
+// BEGIN: using window object (instead of card) to display weather forecast data
+// note: windows allow for me to place UI elements anywhere I'd like
+
+var Vector2 = require('vector2');
+
+// show splash screen while waiting for data
+var splashWindow = new UI.window();
+
+// text element to inform the user
+var text = new UI.Text({
+  position: new Vector2(0, 0),
+  size: new Vector2(144, 168),
+  text: 'Downloading weather data...',
+  font: 'GOTHIC_28_BOLD',
+  color: 'black',
+  textOverflow: 'wrap',
+  textAlign: 'center',
+  backgroundColor: 'white'
 });
 
-// display the card
-card.show();
+// Add to splashWindow and show
+splashWindow.add(text);
+splashWindow.show();
 
-// construct url for the ajax call
 var cityName = 'Orlando';
 var myAPIKey = 'caaf81a949398c16c9f8931faba0562e';
-var URL = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + myAPIKey;
+var URL = 'http://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=' + myAPIKey;
 
-// make the request to the api
+// make the request to openweathermap.org
 ajax(
-{
-  url: URL,
-  type: 'json'
-},
-function(data) {
-//   success
-  console.log('Successfully fetched weather data!');
-  
-//   extract data and adjust temp from Kelvin to Celcius
-  var location = data.name;
-  var temperature = Math.round(data.main.temp - 273.15) + 'C';
-  
-//   always upper-case first letter of description
-  var description = data.weather[0].description;
-  description = description.charAt(0).toUpperCase() + description.substring(1);
-
-//   show data to user by accessing card object
-  card.subtitle(location + ', ' + temperature);
-  card.body(description);
-},
-function(error) {
-//   failure
-  console.log('Failed fetching weather data: ' + error);
-}
+  {
+    url: URL,
+    type: 'json'
+  },
+  function(data){
+    
+  },
+  function(error) {
+    console.log('Download failed: ' + error);
+  }
 );
+
+// END: window object displaying weather forecast
+
+
+// BEGIN: showing wather data for just one city
+// create a card with title and subtitle
+// var card = new UI.Card({
+//   title: 'Weather',
+//   subtitle: 'Fetching...'
+// });
+
+// // display the card
+// card.show();
+
+// // construct url for the ajax call
+// var cityName = 'Orlando';
+// var myAPIKey = 'caaf81a949398c16c9f8931faba0562e';
+// var URL = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + myAPIKey;
+
+// // make the request to the api
+// ajax(
+// {
+//   url: URL,
+//   type: 'json'
+// },
+// function(data) {
+// //   success
+//   console.log('Successfully fetched weather data!');
+  
+// //   extract data and adjust temp from Kelvin to Celcius
+//   var location = data.name;
+//   var temperature = Math.round(data.main.temp - 273.15) + 'C';
+  
+// //   always upper-case first letter of description
+//   var description = data.weather[0].description;
+//   description = description.charAt(0).toUpperCase() + description.substring(1);
+
+// //   show data to user by accessing card object
+//   card.subtitle(location + ', ' + temperature);
+//   card.body(description);
+// },
+// function(error) {
+// //   failure
+//   console.log('Failed fetching weather data: ' + error);
+// }
+// );
+// END: showing wather data for just one city
