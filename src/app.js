ec1f4b80a646343170a59e7131df5e22f428707f
Garrett Lee Graham Anderson <ganderson7197@icloud.com>
2016-01-26 12:53:25 -0500

    finished tutorial
    finished the tutorial by adding the feature for updated weather data when the watch is shaken. the watch should update data when wrist is shaken and then indicate updated data with vibration. now need to figure out how to get the app on the phone and then put some nice styling to it all.

--- src/app.js
+++ src/app.js
@@ -6,13 +6,15 @@
 
 var UI = require('ui');
 var ajax = require('ajax');
-
-// BEGIN: using window object (instead of card) to display weather forecast data
-// note: windows allow for me to place UI elements anywhere I'd like
-
 var Vector2 = require('vector2');
+var Accesl = require('ui/accel');
+var Vibe = require('ui/vibe');
 
- var parseFeed = function(data, quantity) {
+var cityName = 'Orlando';
+var myAPIKey = 'caaf81a949398c16c9f8931faba0562e';
+var URL = 'http://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=' + myAPIKey;
+
+var parseFeed = function(data, quantity) {
       var items = [];
       for(var i = 0; i < quantity; i++) {
 //         always uppercase the description string
@@ -51,10 +53,6 @@ var text = new UI.Text({
 splashWindow.add(text);
 splashWindow.show();
 
-var cityName = 'Orlando';
-var myAPIKey = 'caaf81a949398c16c9f8931faba0562e';
-var URL = 'http://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=' + myAPIKey;
-
 // make the request to openweathermap.org
 ajax(
   {
@@ -80,55 +78,32 @@ ajax(
 //      show the menu, hide the splash
     resultsMenu.show();
     splashWindow.hide();
+    
+//     register for 'tap' events
+    resultsMenu.on('accelTap', function(e){
+//       make another request to openweathermap.org
+      ajax(
+      {
+        url: URL,
+        type: 'json'
+      },
+        function(data){
+//       create an array of menu items
+          var newItems = parseFeed(data, 10);
+          
+//       update the menu's first section
+          resultsMenu.items(0, newItems);
+          
+//       notify the user
+          Vibe.vibrate('short');
+        },
+        function(error){
+          console.log('Download failed: ' + error);  
+        }
+      );
+    });
   },
   function(error) {
     console.log('Download failed: ' + error);
   }
 );
-
-// END: window object displaying weather forecast
-
-
-// BEGIN: showing wather data for just one city
-// create a card with title and subtitle
-// var card = new UI.Card({
-//   title: 'Weather',
-//   subtitle: 'Fetching...'
-// });
-
-// // display the card
-// card.show();
-
-// // construct url for the ajax call
-// var cityName = 'Orlando';
-// var myAPIKey = 'caaf81a949398c16c9f8931faba0562e';
-// var URL = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + myAPIKey;
-
-// // make the request to the api
-// ajax(
-// {
-//   url: URL,
-//   type: 'json'
-// },
-// function(data) {
-// //   success
-//   console.log('Successfully fetched weather data!');
-  
-// //   extract data and adjust temp from Kelvin to Celcius
-//   var location = data.name;
-//   var temperature = Math.round(data.main.temp - 273.15) + 'C';
-  
-// //   always upper-case first letter of description
-//   var description = data.weather[0].description;
-//   description = description.charAt(0).toUpperCase() + description.substring(1);
-
-// //   show data to user by accessing card object
-//   card.subtitle(location + ', ' + temperature);
-//   card.body(description);
-// },
-// function(error) {
-// //   failure
-//   console.log('Failed fetching weather data: ' + error);
-// }
-// );
-// END: showing wather data for just one city
